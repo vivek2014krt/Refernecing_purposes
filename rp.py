5ce116819a6d11fd4da172b1838e72d74a061b12
Vivek Kumar Thakur <85450756+vivek2014krt@users.noreply.github.com>
2024-11-17 15:30:46 +0530

    Update rp.py

--- rp.py
+++ rp.py
@@ -9,3 +9,17 @@ try:
         print("No active Spark session.")
 except NameError:
     print("No Spark session variable exists.")
+
+#spark session with kerberos (just trying)
+spark = (
+    SparkSession.builder
+    .appName("Kerberos_Jupyter_Session")
+    .config("spark.master", "yarn")
+    .config("spark.submit.deployMode", "client")
+    .config("spark.yarn.principal", "your_username@YOUR.REALM.COM")
+    .config("spark.hadoop.fs.defaultFS", "hdfs://your-hdfs-cluster")
+    .config("spark.hadoop.security.authentication", "kerberos")
+    .config("spark.hadoop.security.authorization", "true")
+    .getOrCreate()
+)
+
